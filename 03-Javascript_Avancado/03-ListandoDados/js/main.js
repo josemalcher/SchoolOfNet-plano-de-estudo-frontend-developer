65e6efe5709f78c0c595cdeca963d23258e5911c
José Stélio R. Malcher Junior <contato@josemalcher.net>
2017-11-06 10:02:59 -0300

    03 - Listando dados

---
+++ 03-Javascript_Avancado/03-ListandoDados/js/main.js
@@ -0,0 +1,25 @@
+var list = [
+    {"desc": "rice", "amount": "1", "value": "5.40"},
+    {"desc": "beer", "amount": "12", "value": "1.99"},
+    {"desc": "meat", "amount": "2", "value": "15.00"}
+];
+
+function getTotal(list) {
+    var total = 0;
+    for (var key in list) {
+        total += list[key].value * list[key].amount;
+    }
+    return total;
+}
+
+function setList(list){
+    var table = '<thead><tr><td>Description</td><td>Amount</td><td>Value</td><td>Action</td></tr></thead><tbody>';
+    for(var key in list){
+        table += '<tr><td>'+ list[key].desc +'</td><td>'+ list[key].amount +'</td><td>'+ list[key].value +'</td><td>Edit | Delete</td></tr>';
+    }
+    table += '</tbody>';
+    document.getElementById("listTable").innerHTML = table;
+}
+
+setList(list);
+console.log(getTotal(list))
