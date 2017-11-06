2fcbdfc2c4e8e623bc9a2b1fb9ed02bed7a16f45
José Stélio R. Malcher Junior <contato@josemalcher.net>
2017-11-06 13:17:14 -0300

    09 - Validando formulário

---
+++ 03-Javascript_Avancado/09-ValidandoFormulario/js/config.js
@@ -0,0 +1,9 @@
+function setConfig(){
+    var texts = {
+        "title":"Shopping Control"
+    };
+    document.title = texts.title;
+    document.getElementById("navTitle").innerHTML = texts.title;
+}
+
+setConfig();
