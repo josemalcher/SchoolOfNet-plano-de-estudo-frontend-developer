7815459cfeff910a6aef44440f2968c0187933c9
José Stélio R. Malcher Junior <contato@josemalcher.net>
2018-11-17 11:11:34 -0300

    08 - Arrow functions

---
+++ 11-ES6/8-Arrow-functions.js
@@ -0,0 +1,24 @@
+let array = [1,2,3,4,5];
+
+// let array1 = array.map(function(value){
+//     return value * 10;
+// })
+
+let array2 = array.map(value => value * 10);
+
+let array3 = array.map((value1, value2) => value * 10);
+
+let array4 = array.map(value1 => {
+    value * 10;
+});
+
+array.map(value => value > 10 ? 0 : 2);
+
+let funcaoSoma = (param1, param2) => param1 + param2;
+
+setTimeout(()=> execucao, 100);
+setTimeout(()=>{
+    //execucao
+}), 100);
+
+console.log(array2);
