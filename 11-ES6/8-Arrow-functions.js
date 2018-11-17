2b1d5751274ca92915a79aa33158c63fe4d28952
José Stélio R. Malcher Junior <contato@josemalcher.net>
2018-11-17 11:14:58 -0300

    08 - Arrow functions + Correções

--- 11-ES6/8-Arrow-functions.js
+++ 11-ES6/8-Arrow-functions.js
@@ -6,19 +6,19 @@ let array = [1,2,3,4,5];
 
 let array2 = array.map(value => value * 10);
 
-let array3 = array.map((value1, value2) => value * 10);
+let array3 = array.map((value1, value2) => value1 * 10);
 
 let array4 = array.map(value1 => {
-    value * 10;
+    value1 * 10;
 });
 
 array.map(value => value > 10 ? 0 : 2);
 
 let funcaoSoma = (param1, param2) => param1 + param2;
 
-setTimeout(()=> execucao, 100);
+//setTimeout(()=> execucao, 100);
 setTimeout(()=>{
     //execucao
-}), 100);
+}, 100);
 
 console.log(array2);
